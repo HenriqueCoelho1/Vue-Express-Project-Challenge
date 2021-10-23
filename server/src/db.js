fb9f7412c1aa1e24280d70980e908e52d7f9e615
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-10-23 02:17:11 -0300

    Creating the db connection

---
+++ server/src/db.js
@@ -0,0 +1,12 @@
+const Pool = require("pg").Pool
+require("dotenv").config()
+
+const pool = new Pool = ({
+    user: process.env.ROOT_USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.PORT_POSTGRE,
+    database: process.env.DATABASE
+})
+
+module.exports = pool
---
+++ server/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config()
+const app = express()
+
+app.use(morgan("dev"))
+app.use(cors())
+app.use(express.json())
+
+
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, () => {
+    console.log(`THIS SERVER IS RUNNING IN THE PORT ${PORT}`)
+})
